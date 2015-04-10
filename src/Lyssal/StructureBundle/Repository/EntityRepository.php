d0808bcea8389ad5269ebfb5352bb3d7361a7169
Rémi LECLERC <rleclerc@nodevo.com>
2015-04-10 21:30:08 +0200

    Gestion des innerJoin pour getQueryBuilderFindBy()

--- src/Lyssal/StructureBundle/Repository/EntityRepository.php
+++ src/Lyssal/StructureBundle/Repository/EntityRepository.php
@@ -1,3 +1,4 @@
+
 <?php
 namespace Lyssal\StructureBundle\Repository;
 
@@ -12,25 +13,38 @@ class EntityRepository extends BaseEntityRepository
 {
     /**
      * Retourne le QueryBuilder pour la méthode findBy().
-     * 
+     *
      * @param array $conditions Conditions de la recherche
      * @param array|NULL $orderBy Tri des résultats
      * @param integer|NULL $limit Limite des résultats
      * @param integer|NULL $offset Offset
      * @return \Doctrine\ORM\QueryBuilder QueryBuilder
      */
-    public function getQueryBuilderFindBy(array $conditions, array $orderBy = null, $limit = null, $offset = null)
+    public function getQueryBuilderFindBy(array $conditions, array $orderBy = null, $limit = null, $offset = null, array $extras = array())
     {
         $requete = $this->createQueryBuilder('entite');
-        
+
+        if (isset($extras['innerJoins']))
+        {
+            foreach ($extras['innerJoins'] as $innerJoin => $innerJoinAlias)
+            {
+                if (false === strpos($innerJoin, '.'))
+                    $requete->innerJoin('entite.'.$innerJoin, $innerJoinAlias);
+                else $requete->innerJoin($innerJoin, $innerJoinAlias);
+            }
+        }
+
         foreach ($conditions as $conditionPropriete => $conditionValeur)
         {
-            $requete
-                ->andWhere('entite.'.$conditionPropriete.' = :'.$conditionPropriete)
-                ->setParameter($conditionPropriete, $conditionValeur)
-            ;
+            $conditionValeurLabel = str_replace('.', '_', $conditionPropriete);
+
+            if (false === strpos($conditionPropriete, '.'))
+$requete->andWhere('entite.'.$conditionPropriete.' = :'.$conditionValeurLabel);
+            else $requete->andWhere($conditionPropriete.' = :'.$conditionValeurLabel);
+
+            $requete->setParameter($conditionValeurLabel, $conditionValeur);
         }
-        
+
         if (null !== $orderBy)
         {
             foreach ($orderBy as $propriete => $orderSens)
@@ -40,13 +54,13 @@ class EntityRepository extends BaseEntityRepository
                 else $requete->addOrderBy('entite.'.$propriete, $orderSens);
             }
         }
-        
+
         if (null !== $limit)
             $requete->setMaxResults($limit);
-        
+
         if (null !== $offset)
             $requete->setFirstResult($offset);
-        
+
         return $requete->getQuery();
     }
 
@@ -59,9 +73,9 @@ class EntityRepository extends BaseEntityRepository
      * @param integer $currentPage Page à afficher
      * @return \Pagerfanta\Pagerfanta Pagerfanta
      */
-    public function getPagerFantaFindBy(array $conditions, array $orderBy = null, $nombreResultatsParPage = 20, $currentPage = 1)
+    public function getPagerFantaFindBy(array $conditions, array $orderBy = null, $nombreResultatsParPage = 20, $currentPage = 1, array $extras = array())
     {
-        $adapter = new \Pagerfanta\Adapter\DoctrineORMAdapter($this->getQueryBuilderFindBy($conditions, $orderBy));
+        $adapter = new \Pagerfanta\Adapter\DoctrineORMAdapter($this->getQueryBuilderFindBy($conditions, $orderBy, null, null, $extras));
         $pagerFanta = new \Pagerfanta\Pagerfanta($adapter);
         $pagerFanta->setMaxPerPage($nombreResultatsParPage);
         $pagerFanta->setCurrentPage($currentPage);
